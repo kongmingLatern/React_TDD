58cab580bf00d82b5758b7cf5cc2acc0496aeec0
凤之兮原 <1577310596@qq.com>
2022-10-11 19:15:12 +0800

    chore: add daisyUI

--- react-tdd/src/App.tsx
+++ react-tdd/src/App.tsx
@@ -1,11 +1,6 @@
-import './App.css'
-
 function App() {
-
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <button className="btn">Button</button>
   )
 }
 
--- react-tdd/tailwind.config.js
+++ react-tdd/tailwind.config.js
@@ -1,4 +1,6 @@
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
-  //...
+  // content: ["./src/**/*.{html,js,jsx, ts, tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require("daisyui")],
 }
