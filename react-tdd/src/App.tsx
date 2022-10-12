593365ddf754294bb57b3bff2157e1be7cee0066
凤之兮原 <1577310596@qq.com>
2022-10-12 22:01:13 +0800

    change: chnage app

--- react-tdd/src/App.tsx
+++ react-tdd/src/App.tsx
@@ -1,38 +1,29 @@
 function App() {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input type="text" placeholder="email" className="input input-bordered" />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input type="text" placeholder="password" className="input input-bordered" />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </div>
-        </div>
+    <div
+      className="form-control w-50
+      sm:flex sm:justify-center sm:align-middle sm:w-80 sm:mx-auto sm:m-0
+      md:flex md:justify-center md:align-middle md:w-30 md:mx-auto md:m-0
+    "
+    >
+      <div className="input-group">
+        <input type="text" placeholder="Search…" className="input input-bordered" />
+        <button className="btn btn-square">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   )
